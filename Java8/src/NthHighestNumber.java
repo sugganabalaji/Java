71bc20e2e61ab88bab4697b915b89e96d14c5054
Balaji <sugganabalaji@gmail.com>
2025-08-02 01:31:17 +0530

    N-th Highest Number program

---
+++ Java8/src/NthHighestNumber.java
@@ -0,0 +1,38 @@
+/**
+ * Write a Java program to find the second-highest number in a given array.
+ */
+package com.app;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class NthHighestNumber {
+
+    /**
+     * What is .boxed() in Java Streams?
+     * <p>
+     * The boxed() method in Java Streams converts a primitive stream
+     * (like IntStream, LongStream, or DoubleStream) into a stream of their corresponding wrapper class objects
+     * (e.g., Stream<Integer>, Stream<Long>, Stream<Double>).
+     *
+     */
+    private static Integer findNthHighest(int[] intArr, int n) {
+        if (n == 0) {
+            return null;
+        }
+        return Arrays.stream(intArr) // Converts the array to a stream
+                .boxed()
+                .sorted(Comparator.reverseOrder()) // sorts in reverse order
+                .skip(n - 1) // skips till (n-1) index elements.
+                .findFirst() // retrieves the n-th Highest value
+                .orElse(null);
+    }
+
+    public static void main(String[] args) {
+        int[] input = {5, 9, 2, 13, 42, 56, 24, 36};
+        int n = 2;
+        Integer secondHighest = findNthHighest(input, n);
+        System.out.println(secondHighest); // 42
+    }
+
+}
