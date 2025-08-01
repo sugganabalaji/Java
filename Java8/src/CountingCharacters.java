8f53b9612800a896ad6258e125c8b35149e487d7
Balaji <sugganabalaji@gmail.com>
2025-08-02 00:47:05 +0530

    Counting Characters program

---
+++ Java8/src/CountingCharacters.java
@@ -0,0 +1,23 @@
+/**
+ * Given a string, write a program to count the occurrences of each character.
+ */
+package com.app;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountingCharacters {
+
+    public static Map<Character, Long> countCharacters(String str) {
+        return str.chars()  // Convert string to a stream of char values (int)
+                .mapToObj(c -> (char) c) // Convert int values back to chars
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        Map<Character, Long> freqMap = countCharacters(s);
+        System.out.println(freqMap); // {e=1, H=1, l=2, o=1}
+    }
+}
