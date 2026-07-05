4900b71a6c4e3466d752a3e9151b8403ca4d50e7
Balaji, Suggana <sugganabalaji@gmail.com>
2026-07-05 15:16:55 +0530

    Print sum of all numbers

--- src/com/stream/numbers/Program1.java
+++ src/com/stream/numbers/Program1.java
@@ -8,8 +8,14 @@ public class Program1 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 45, 46, 555, 3, 44, 55, 49);
 
-        // Print sum of all numbers
-        int sum = numbers.stream().reduce((a, b) -> a+b).get();
+        // printing sum of all numbers
+        //1+2=3
+        //3+3=6
+        //6+45=51
+        // ................
+
+        int sum = numbers.stream().reduce((a,b)->a+b).get();
+
         System.out.println(sum);
 
     }
