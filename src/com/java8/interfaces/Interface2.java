c56caf4d68f611cc3c821cbcf904bd0a102f600b
Balaji, Suggana <sugganabalaji@gmail.com>
2026-07-05 14:43:42 +0530

    Functional interfaces

--- src/com/java8/interfaces/Interface2.java
+++ src/com/java8/interfaces/Interface2.java
@@ -3,5 +3,8 @@ package com.java8.interfaces;
 @FunctionalInterface
 public interface Interface2 extends Interface1 {
 
+    // only one abstract method
+    // n number of default and static methods
+    // can not extend another functional interface. always make sure SAM by inheritance.
 
 }
