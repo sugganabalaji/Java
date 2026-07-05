4290180953b8bcede4a56c4c9d15fc34f6ee5db2
Balaji, Suggana <sugganabalaji@gmail.com>
2026-07-05 16:22:24 +0530

    How many male and female employees in IT department

---
+++ src/com/stream/pojos/Program8.java
@@ -0,0 +1,30 @@
+package com.stream.pojos;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Program8 {
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee("Sai", "HR", 45000, "Male", LocalDate.of(2010, 5, 05));
+        Employee emp2 = new Employee("Krishna", "Accounting", 45000, "Male", LocalDate.of(2012, 2, 02));
+        Employee emp3 = new Employee("Anusha", "IT", 60000, "Female", LocalDate.of(2019, 9, 23));
+        Employee emp4 = new Employee("Ashok", "IT", 50000, "Male", LocalDate.of(2013, 5, 20));
+
+        List<Employee> employees = Arrays.asList(emp, emp2, emp3, emp4);
+
+        // count of male and female in IT department
+
+        Map<String, Long> collect3 = employees.stream()
+                .filter(e -> e.getDepartment().equalsIgnoreCase("IT"))
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+
+        System.out.println(collect3);
+    }
+
+}
