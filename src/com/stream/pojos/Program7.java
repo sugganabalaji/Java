29ae388b734cbbd71a2b23dab6369fafe60fa119
Balaji, Suggana <sugganabalaji@gmail.com>
2026-07-05 16:19:18 +0530

    Most Senior in the Organization

---
+++ src/com/stream/pojos/Program7.java
@@ -0,0 +1,26 @@
+package com.stream.pojos;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Program7 {
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee("Sai", "HR", 45000, "Male", LocalDate.of(2010, 5, 05));
+        Employee emp2 = new Employee("Krishna", "Accounting", 45000, "Male", LocalDate.of(2012, 2, 02));
+        Employee emp3 = new Employee("Anusha", "IT", 60000, "Female", LocalDate.of(2019, 9, 23));
+        Employee emp4 = new Employee("Ashok", "IT", 50000, "Male", LocalDate.of(2013, 5, 20));
+
+        List<Employee> employees = Arrays.asList(emp, emp2, emp3, emp4);
+
+        // Most senior employee
+
+        Employee e = employees.stream().min(Comparator.comparing(Employee::getJoiningDate)).get();
+
+        System.out.println(e);
+    }
+
+}
