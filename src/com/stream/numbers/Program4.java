225b5ef0b656adb7d62c00613e77d4234056d394
Balaji, Suggana <sugganabalaji@gmail.com>
2026-07-05 15:24:28 +0530

    Print numbers starts with four

---
+++ src/com/stream/numbers/Program4.java
@@ -0,0 +1,22 @@
+package com.stream.numbers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program4 {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 45, 46, 555, 3, 44, 55, 49);
+
+        // numbers starts with four
+
+        List<String> list = numbers.stream()
+                .map(n -> n.toString())
+                .filter(n->n.startsWith("4"))
+                .toList();
+
+        System.out.println(list);
+
+    }
+
+}
